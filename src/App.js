738c89eb5e60b8af6c4c3e18bac6824a5d355389
Alexandr <poloalexand@mail.ru>
2023-10-26 16:17:34 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    CheckboxInput,
+    EmailInput,
+    PlainButton,
+    RadioInput,
+    SearchInput,
+} from "@prometey/ui-kit-test-lib";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const buttons = [];
+    const inputs = [];
+    const searchInput = [];
+    const checkboxInput = [];
+    const radioInput = [];
+
+    for (let i = 1; i <= 500; i++) {
+        buttons.push(<PlainButton buttonText="Button" key={i} />);
+    }
+
+    for (let i = 1; i <= 500; i++) {
+        inputs.push(<EmailInput placeholder="Basic usage" key={i} />);
+    }
+    for (let i = 1; i <= 500; i++) {
+        searchInput.push(<SearchInput key={i} />);
+    }
+    for (let i = 1; i <= 500; i++) {
+        checkboxInput.push(<CheckboxInput key={i} />);
+    }
+    for (let i = 1; i <= 500; i++) {
+        radioInput.push(<RadioInput key={i} />);
+    }
+    return (
+        <div className="App">
+            {buttons}
+            {inputs}
+            {searchInput}
+            {checkboxInput}
+            {radioInput}
+        </div>
+    );
 }
 
 export default App;
